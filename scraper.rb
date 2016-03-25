5e5df5baadc440598721e5f97b762566cf25eed9
Tony Bowden <tony@tmtm.com>
2016-03-25 12:48:45 +0000

    Use webhook instead of notifier

--- scraper.rb
+++ scraper.rb
@@ -5,4 +5,3 @@ require 'wikidata/fetcher'
 
 names = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/puerto-rico-house-of-representatives-29', column: 'wikiname')
 EveryPolitician::Wikidata.scrape_wikidata(names: { en: names }, output: false)
-warn EveryPolitician::Wikidata.notify_rebuilder
