ac59768ff71d29dccd63f1c38265cf9dacf52004
Tony Bowden <tony@tmtm.com>
2017-01-09 09:47:42 +0000

    Add Commission Members

--- scraper.rb
+++ scraper.rb
@@ -3,11 +3,20 @@
 
 require 'wikidata/fetcher'
 
-names29 = EveryPolitician::Wikidata.wikipedia_xpath(
+members29 = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/29th_House_of_Representatives_of_Puerto_Rico',
   after: '//span[@id="Composition"]',
   before: '//span[@id="Commissions"]',
   xpath: './/table//tr[td]//td[2]//a[not(@class="new")][1]/@title',
+)
+
+# Some members who resign and aren't in the members table are still
+# listed in the Commissions table
+commission29 = EveryPolitician::Wikidata.wikipedia_xpath(
+  url: 'https://en.wikipedia.org/wiki/29th_House_of_Representatives_of_Puerto_Rico',
+  after: '//span[@id="Commissions"]',
+  before: '//span[@id="Notable_legislations"]',
+  xpath: './/table//tr[td]//td//a[not(@class="new")]/@title',
 )
 
 names30 = EveryPolitician::Wikidata.wikipedia_xpath(
@@ -17,4 +26,4 @@ names30 = EveryPolitician::Wikidata.wikipedia_xpath(
   xpath: './/table//tr[td]//td[2]//a[not(@class="new")][1]/@title',
 )
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names29 | names30 })
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: members29 | commission29 | names30 })
